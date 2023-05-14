b20c39d7fde9d616e7feb0f5a569919e948fd963
Harineko0 <harineko0927@gmail.com>
2023-05-14 11:41:52 +0900

    init git

---
+++ README.md
@@ -0,0 +1,3 @@
+# Firebase ML チュートリアル
+## 環境構築
+[参考サイト](https://7nohe-tech-blog.vercel.app/post/flutter-firebase-ocr)
---
+++ android/app/src/main/kotlin/jp/mincra/firebase_ml_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package jp.mincra.firebase_ml_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
